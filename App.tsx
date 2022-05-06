237e897b93208848d3ce60d62b4595f832eb92e1
Sinan Vanli <sinan.vanli@gmail.com>
2022-05-06 20:41:15 +0100

    messing around on git

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>ZingBong</Text>
+      <Text>Test<Text/>
       <StatusBar style="auto" />
     </View>
   );
