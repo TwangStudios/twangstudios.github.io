1247c05f1c4c2d974615fb95e11797b1575b50e4
alexsidell <hockeysid@gmail.com>
2022-05-06 21:00:22 +0100

    This is a test

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>ZingBong</Text>
-      <Text>Test<Text/>
+      <Text>🍺<Text/>
       <StatusBar style="auto" />
     </View>
   );
