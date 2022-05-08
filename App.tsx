829404ea0a03f9e17bf1c304d9ce1fa4099a2a37
Sinan Vanli <sinan.vanli@gmail.com>
2022-05-08 14:47:46 +0100

    Implemented saving

--- App.tsx
+++ App.tsx
@@ -2,13 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import DrawModal from './drawModal/drawModal';
+import Select from 'react-select';
 
 export default function App() {
+  const [selectedOption, setSelectedOption] = useState<string|undefined>(undefined);
+
+
+  function getItems() {
+
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push({value: key, label: JSON.parse(localStorage.getItem(key)!).name});
+    }
+
+    return archive;
+}
+
+  const items = getItems();
+
   return (
     <View style={styles.container}>
       <Text>ZingBong</Text>
       <Text>🍺 Cheers!</Text>
       <StatusBar style="auto" />
+      <Select 
+        options={items}
+        onChange={(item) => setSelectedOption(item?.value)}
+      />
       <DrawModal />
     </View>
   );
