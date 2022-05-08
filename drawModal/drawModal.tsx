4f67fdb1694ed2966316abc1496c8d616b5518be
alexsidell <hockeysid@gmail.com>
2022-05-08 17:40:26 +0100

    Description

--- drawModal/drawModal.tsx
+++ drawModal/drawModal.tsx
@@ -3,6 +3,7 @@ import ReactModal from "react-modal";
 import './drawModal';
 import CanvasDraw from "react-canvas-draw";
 import * as uuid from "uuid";
+import '../styles.tsx';
 
 ReactModal.setAppElement('body');
 
@@ -69,9 +70,13 @@ export default function DrawModal({id:editId, onSave}: ItemId & SaveAction) {
                 isOpen={modalIsOpen}
                 contentLabel="Minimal Modal Example"
             >
-                <CanvasDraw ref={canvas} saveData={imageInit}/>
                 <button onClick={closeModal}>close</button>
+                <br/>
+                <CanvasDraw ref={canvas} saveData={imageInit}/>
                 <input placeholder={name || "What's the item called?"} onBlur={(e) => checkName(e.target.value)}/>
+                <br/>
+                <textarea placeholder={"Item Description"}/>
+                <br/>
                 <button onClick={save}>Save</button>
                 <button onClick={closeModal}>Cancel</button>
             </ReactModal>
---
+++ styles.tsx
@@ -0,0 +1,10 @@
+const defualtCanvas = {
+    content: {
+    width: '85%',
+    height: '85%',
+    }
+}
+
+const myStyle = {
+    
+}
